caa2f6f205d97390f196b46f38622ffdd9896208
dvherrera <dvherrera@yahoo.com>
2015-11-09 23:18:08 +0100

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -2,6 +2,7 @@
 
 class Cluster:
     lista_nodos = []
+    lista_nombre_nodos = []
     lista_nodos_externos = []
     lista_nodos_internos = []
 
@@ -16,12 +17,18 @@ class Cluster:
             print(kk.dime_nombre())
     
     def coloca_nodos(self):
+        lista_comprobacion = self.nombre_lista_nodos()
         for kk in self.lista_nodos:
             valor=True
             for m in kk.vecinos():
                 if m not in self.lista_nodos:
                     valor=False
             if valor == True:
-                    lista_nodos_internos.append(kk)
+                    self.lista_nodos_internos.append(kk)
             else:
-                    lista_nodos_externos.append(kk)
+                    self.lista_nodos_externos.append(kk)
+
+    def nombre_lista_nodos():
+        for kk in self.lista_nodos;
+            self.lista_nombre_nodos.append(kk.dime_nombre())
+        return self.lista_nombre_nodos
